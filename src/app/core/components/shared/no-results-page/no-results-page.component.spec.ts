b521187a6040984d17626150bfcb3216083ec746
darynkka <dashalitviniuk20@gmail.com>
2025-09-01 17:21:29 +0300

    primary architecture impl

---
+++ src/app/core/components/shared/no-results-page/no-results-page.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NoResultsPageComponent } from './no-results-page.component';
+
+describe('NoResultsPageComponent', () => {
+  let component: NoResultsPageComponent;
+  let fixture: ComponentFixture<NoResultsPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [NoResultsPageComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(NoResultsPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
